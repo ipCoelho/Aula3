0d90c91dfc81ec00487b381b4fb1d5de68c9f772
ISRAEL PABLO COELHO <21193386@CFP127ALUNO.LOCAL>
2021-08-17 16:33:00 -0300

    calculadora imc - pronta

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // variáveis
-const nome = document.getElementById("nome")
+const inputNome = document.getElementById("valueNome")
 const peso = document.getElementById("valuePeso")
 const altura = document.getElementById("valueAltura")
 let resultado = document.getElementById("resultado")
@@ -7,13 +7,40 @@ let resultado = document.getElementById("resultado")
 // botões
 const btnCalcular = document.getElementById("botao")
 
-//funções
+// funções
 function calcularImc() {
-    imc = peso.value / altura.value ** 2
-    resultado.textContent = "O seu imc é: "+imc
+    var imc = peso.value / altura.value ** 2
+    imc.toFixed(2)
+    nome = inputNome.value
+    if (imc < 18.5) {
+        resultado.textContent = nome+", seu IMC é "+imc+". "+"Você está abaixo do peso."
+    } else { 
+        if (imc >= 18.5 && imc <= 24.9) {
+            resultado.textContent = nome+", seu IMC é "+imc+". "+"Parabéns! Você está no peso ideal."
+        } else {
+            if (imc >= 25 && imc <= 29.9) {
+                resultado.textContent = nome+", seu IMC é "+imc+". "+"Você está acima do peso."
+            } else {
+                if (imc >= 30 && imc <= 34.9) {
+                    resultado.textContent = nome+", seu IMC é "+imc+". "+"Cuidado. Você está em obesidade I."
+                } else {
+                    if (imc >= 35 && imc <= 39.9) {
+                        resultado.textContent = nome+", seu IMC é "+imc+". "+"Cuidado. Você está em obesidade II."
+                    } else {
+                        if (imc > 40) {
+                            resultado.textContent = nome+", seu IMC é "+imc+". "+"Cuidado. Você está em obesidade III."
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    
+    
 }
 
-//botao-evento
-btnCalcular.addEventListener("click", calcularImc)
 
-console.log()
+
+// Adicionando um evento ao botão
+btnCalcular.addEventListener("click", calcularImc)
